4bfcef04f4b38efdcf53d323471f89141164cc87
alx-s <saunier.alexandre@yahoo.com>
2013-03-16 22:20:40 +0100

    OSC_python-pd : correction

--- OSC_python-pd/OSC_send_python.py
+++ OSC_python-pd/OSC_send_python.py
@@ -8,7 +8,7 @@ client = OSC.OSCClient()
 client.connect(('127.0.0.1', 9001)) # first argument is the IP of the host, second argument the port to use
 
 try:
-	client.send(OSC.OSCMessage("/adress", data) # first argument is the what we could call the OSC adress of the data, second argument is the actual data to be sent.
+	client.send(OSC.OSCMessage("/adress", data)) # first argument is the what we could call the OSC adress of the data, second argument is the actual data to be sent.
 except:
 	print "not connected"
         pass
